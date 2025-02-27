89f281403e679c034fe439b4340a03753c580be7
vladimir <vladimir.dilanyan@raisin.com>
2025-02-27 16:30:41 +0400

    refactor: code style

--- functions/theme-support.php
+++ functions/theme-support.php
@@ -1,6 +1,6 @@
 <?php
-add_theme_support("post-thumbnails");
-add_image_size("blog-thumb", 200, 200, true);
+add_theme_support('post-thumbnails');
+add_image_size('blog-thumb', 200, 200, true);
 
 function get_homepage_fields(): array
 {
@@ -20,17 +20,17 @@ function get_homepage_fields(): array
 function add_open_graph_meta_tags(): void
 {
     global $post;
+
     $custom_image_url = get_stylesheet_directory_uri() . '/assets/img/logo.jpg';
-    $publish_date = get_the_date(DATE_W3C, $post->ID);
-    ?>
-    <meta property="og:title" content="<?php echo esc_attr(get_the_title($post->ID)); ?>"/>
-    <meta property="og:description" content="<?php echo esc_attr(get_field('about_me_description', $post->ID)); ?>"/>
-    <meta property="og:url" content="<?php echo esc_url(get_permalink($post->ID)); ?>"/>
-    <meta property="og:image" content="<?php echo esc_url($custom_image_url); ?>"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:site_name" content="<?php bloginfo('name'); ?>"/>
-    <meta property='article:published_time' content="<?php echo esc_attr($publish_date); ?>"/>
-    <?php
+    $publish_date = get_the_date(DATE_W3C, $post->ID); ?>
+
+    <meta property="og:title" content="<?php echo esc_attr(get_the_title($post->ID)); ?>" />
+    <meta property="og:description" content="<?php echo esc_attr(get_field('about_me_description', $post->ID)); ?>" />
+    <meta property="og:url" content="<?php echo esc_url(get_permalink($post->ID)); ?>" />
+    <meta property="og:image" content="<?php echo esc_url($custom_image_url); ?>" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
+    <meta property="article:published_time" content="<?php echo esc_attr($publish_date); ?>" /> <?php
 }
 
 add_action('wp_head', 'add_open_graph_meta_tags');
